6c01c7024742a212ffaad19e343092a285e668da
doomout <winquake3@hanmail.net>
2022-12-21 01:58:17 +0900

    아웃렛 변수와 액션 함수 추가

--- Alert/ViewController.swift
+++ Alert/ViewController.swift
@@ -1,19 +1,23 @@
-//
-//  ViewController.swift
-//  Alert
-//
-//  Created by 현구김 on 2022/12/20.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var lamplmg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func btnLampOn(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnLampOff(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnLampRemove(_ sender: UIButton) {
+    }
 }
 
